f9d442058e8795020fce6858482442be882f40cb
JonahSussman <sussmanjonah@gmail.com>
2020-12-24 23:33:34 -0500

    Finished search w/ responsive ui

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,3 @@
-// Random profile pic
-document.getElementById('tw-nav-profile').src = 
-  'assets/avatar' + String(Math.floor(Math.random() * 7) + 1) + '.png';
+function loaded() {
+  document.getElementById('tw-nav-profile').src = 'assets/avatar' + String(Math.floor(Math.random() * 7) + 1) + '.png';
+}
